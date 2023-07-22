aa7a2dcd0f76ef026e0bcb59e4125dadf0f2e7af
Nicholas J. Michalek <djphazer@gmail.com>
2023-07-22 01:29:05 -0400

    Build config, script tweaks for Windows
    * Upgrade to platform-teensy v4.17.0
      - DMAChannel.h generates warnings/errors in v4.18.0, so we'll park it here.
    * Stuck "phazerville" in the filename - this is about to be more than just drivers!
    * Enable USB_MIDI - we're gonna need it

--- scripts/progname.py
+++ scripts/progname.py
@@ -2,7 +2,10 @@ Import("env")
 
 import subprocess
 
-version = subprocess.check_output("./scripts/build_version.sh", shell=True).decode().strip()
+version = subprocess.check_output("sh ./scripts/build_version.sh", shell=True).decode().strip()
 git_rev = subprocess.check_output("git rev-parse --short HEAD", shell=True).decode().strip()
 
-env.Replace(PROGNAME=f"oCt4-{version}-{git_rev}")
+env.Append(BUILD_FLAGS=[ f'-DOCT4_BUILD_VERSION=\\"{version}\\"',
+                         f'-DOCT4_BUILD_TAG=\\"{git_rev}\\"' ])
+
+env.Replace(PROGNAME=f"oCt4-phazerville-{version}-{git_rev}")
